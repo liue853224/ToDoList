fb33c5ba5e5686e53279a78c783a8a0bf8c6cd10
liue853224@gmail.com <liue853224@gmail.com>
2024-07-02 21:12:46 +0800

    Route init

--- app.js
+++ app.js
@@ -2,6 +2,37 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+// Route
+app.get("/", (req, res) => {
+  res.send("It works!");
+});
+app.get("/todos", (req, res) => {
+  res.send("get all todos");
+});
+
+app.get("/todos/new", (req, res) => {
+  res.send("create todo");
+});
+
+app.post("/todos", (req, res) => {
+  res.send("add todo");
+});
+
+app.get("/todos/:id", (req, res) => {
+  res.send(`get todo: ${req.params.id}`);
+});
+
+app.get("/todos/:id/edit", (req, res) => {
+  res.send(`get todo edit: ${req.params.id}`);
+});
+
+app.put("/todos/:id", (req, res) => {
+  res.send("modify todo");
+});
+
+app.delete("/todos/:id", (req, res) => {
+  res.send("delete todo");
+});
 // 伺服器監聽
 app.listen(port, () => {
   console.log(`The server is running on port:${port}`);
