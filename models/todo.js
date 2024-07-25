8dcb8a6e762a1600895908fe89a8c57fa522392e
liue853224@gmail.com <liue853224@gmail.com>
2024-07-25 20:52:55 +0800

    add user-todo relationship migrations

--- models/todo.js
+++ models/todo.js
@@ -8,7 +8,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Todo.belongsTo(models.User);
     }
   }
   Todo.init(
@@ -17,6 +17,10 @@ module.exports = (sequelize, DataTypes) => {
       isComplete: {
         type: DataTypes.BOOLEAN,
         defaultValue: false,
+        allowNull: false,
+      },
+      userId: {
+        type: DataTypes.INTEGER,
         allowNull: false,
       },
     },
